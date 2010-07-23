80d1e73ff17a67629156c9158c6f2fc9ae061a1a
lizconlan <liz@work.machine>
2010-07-23 11:41:54 +0100

    new and improved with an actual spec

--- spec/deposited_paper_spec.rb
+++ spec/deposited_paper_spec.rb
@@ -1,5 +1,13 @@
 require File.dirname(__FILE__) + '/spec_helper'
+require 'lib/deposited_paper'
 
 describe "DepositedPaper" do
-  it "should do stuff"
+  describe "when creating a new object" do
+    it "should set the reference property" do
+      ref = "DEP1879-001"
+      paper = DepositedPaper.new(ref)
+    
+      paper.reference.should == ref
+    end
+  end
 end
