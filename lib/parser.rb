04cc7bc46f451608fe3a76a3f2d66abb0458c49d
lizconlan <lizconlan@gmail.com>
2010-11-04 18:48:25 +0000

    splitting department names into arrays

--- lib/parser.rb
+++ lib/parser.rb
@@ -44,7 +44,7 @@ class Parser
             end
             paper.legislature = cells[1].text
             paper.deposited_date = cells[2].text
-            paper.department = cells[3].text
+            paper.department = parse_departments(cells[3].text)
             paper.link_to_paper = cells[4].xpath('a').attribute('href').value
           end
         else
@@ -79,7 +79,7 @@ class Parser
               when "date"
                 paper.deposited_date = cell.text
               when "dept"
-                paper.department = cell.text
+                paper.department = parse_departments(cell.text)
               when "link"
                 paper.link_to_paper = cell.xpath('a').attribute('href').value
               when "des"
@@ -93,6 +93,111 @@ class Parser
         end
       end
       paper.save if paper
+    end
+  
+    def parse_departments(text)
+      #always have Department in full
+      text = text.gsub("Dept.", "Department").gsub("Dept", "Department")
+      
+      #split on Department of/for
+      text = text.gsub(" Department of", "|Department of").gsub(" Department for", "|Department for")
+      
+      #split on Ministry of/for
+      text = text.gsub(" Ministry of", "|Ministry of").gsub(" Ministry for", "|Ministry for")
+      
+      #split on Minister of/for
+      text = text.gsub(" Minister of", "|Minister of").gsub(" Minister for", "|Minister for")
+      
+      #split on Office of/for
+      text = text.gsub(" Office of", "|Office of").gsub(" Office for", "|Office for")
+      #repair any instances of Central or Government Office that got broken by the last line
+      text = text.gsub("Central|Office", "Central Office")
+      text = text.gsub("Government|Office", "Government Office")
+      
+      #split after Agency
+      text = text.gsub("Agency ", "Agency|")
+      text = text.gsub("Agency|Wales", "Agency Wales")
+      text = text.gsub("Agency|Northern Ireland", "Agency Northern Ireland")
+      text = text.gsub("Agency|Scotland", "Agency Scotland")
+      text = text.gsub("Agency|for", "Agency for")
+      
+      #split on Chief
+      text = text.gsub(" Chief", "|Chief")
+      text = text.gsub(" of|Chief", " of Chief")
+      
+      #split on HM
+      text = text.gsub(" HM ", "|HM ")
+      
+      #split after Speaker
+      text = text.gsub(/[s|S]peaker /i, "Office of the Speaker|")
+      text = text.gsub("Office of the Office of the", "Office of the")
+      
+      #split after Prime Minister
+      text = text.gsub("Prime Minister ", "Prime Minister|")
+      
+      #split after Treasury
+      text = text.gsub("Treasury ", "Treasury|")
+      text = text.gsub(" Treasury", "|Treasury")
+      
+      #split after Cabinet Office
+      text = text.gsub("Cabinet Office ", "Cabinet Office|")
+      #split on Cabinet Office
+      text = text.gsub(" Cabinet Office", "|Cabinet Office")
+      
+      #split after Whips Office
+      text = text.gsub("Whips Office ", "Whips Office|")
+      
+      #handle specific Offices
+      text = text.gsub("Foreign and Commonwealth Office ", "Foreign and Commonwealth Office|")
+      text = text.gsub(" Foreign and Commonwealth Office", "|Foreign and Commonwealth Office")
+      text = text.gsub("Home Office ", "Home Office|")
+      text = text.gsub(" Home Office", "|Home Office")
+      text = text.gsub("Northern Ireland Office ", "Northern Ireland Office|")
+      text = text.gsub(" Northern Ireland Office", "|Northern Ireland Office")
+      text = text.gsub("Office for National Statistics ", "Office for National Statistics|")
+      text = text.gsub("Office for Scotland ", "Office for Scotland|")
+      
+      #handle specific cases
+      text = text.gsub(/CLERK OF THE PARLIAMENTS /i, "Clerk of the Parliaments|")
+      text = text.gsub("Communities and Local Government ", "Communities and Local Government|")
+      text = text.gsub("Council of Europe ", "Council of Europe|")
+      text = text.gsub(" Crown Prosecution Service", "|Crown Prosecution Service")
+      text = text.gsub("Crown Prosecution Service ", "Crown Prosecution Service|")
+      text = text.gsub(" Law Commission", "|Law Commission")
+      text = text.gsub("Ministry of Defence ", "Ministry of Defence|")
+      text = text.gsub("Ministry of Justice ", "Ministry of Justice|")
+      text = text.gsub(" National Assembly for Wales", "|National Assembly for Wales")
+      text = text.gsub(" Speakers Committee on", "|Speakers Committee on")
+      text = text.gsub("United Kingdom Statistics Authority ", "United Kingdom Statistics Authority|")
+      text = text.gsub(" United Kingdom Statistics Authority", "|United Kingdom Statistics Authority")
+      text = text.gsub("Welsh Assembly ", "Welsh Assembly|")
+      text = text.gsub(" Welsh Assembly ", "|Welsh Assembly")
+      
+      text = text.gsub("Department for Business Enterprise and Regulatory Reform ", "Department for Business, Enterprise and Regulatory Reform|")
+      text = text.gsub("Department for Business, Enterprise and Regulatory Reform ", "Department for Business, Enterprise and Regulatory Reform|")
+      text = text.gsub("Department for Business, Innovation and Skills ", "Department for Business, Innovation and Skills|")
+      text = text.gsub("Department for Children Schools and Families ", "Department for Children, Schools and Families|")
+      text = text.gsub("Department for Children, Schools and Families ", "Department for Children, Schools and Families|")
+      text = text.gsub("Department for Culture Media and Sport ", "Department for Culture, Media and Sport|")
+      text = text.gsub("Department for Culture, Media and Sport ", "Department for Culture, Media and Sport|")
+      text = text.gsub("Department for Education ", "Department for Education|")
+      text = text.gsub("Department of Energy and Climate Change ", "Department of Energy and Climate Change|")
+      text = text.gsub("Department for Environment, Food and Rural Affairs ", "Department for Environment, Food and Rural Affairs|")
+      text = text.gsub("Department of Health ", "Department of Health|")
+      text = text.gsub("Department for Innovation, Universities and Skills ", "Department for Innovation, Universities and Skills|")
+      text = text.gsub("Department for International Development ", "Department for International Development|")
+      text = text.gsub("Department for Transport ", "Department for Transport|")
+      text = text.gsub("Department for Work and Pensions ", "Department for Work and Pensions|")
+      
+      #generic fixes
+      text = text.gsub("|Office|", "|Office ")
+      text = text.gsub(/\|Government(\||$)/, " Government")
+      text = text.gsub("|and", " and")
+      
+      text = text.gsub("\n", "|") #more in hope than expectation
+      
+      #remove duplicate | chars and split into array
+      text.squeeze("|").split("|")
     end
   
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -72,7 +72,7 @@ describe "Parser" do
         
         @paper.should_receive(:legislature=).with("Commons, Lords")
         @paper.should_receive(:deposited_date=).with("21/07/2010")
-        @paper.should_receive(:department=).with("Department for Transport")
+        @paper.should_receive(:department=).with(["Department for Transport"])
         @paper.should_receive(:link_to_paper=).with("http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip")
         @paper.should_receive(:description=).with("Description goes here")
         @paper.should_receive(:legislation=).with("")
@@ -111,7 +111,7 @@ describe "Parser" do
         
         @paper.should_receive(:legislature=).with("Commons, Lords")
         @paper.should_receive(:deposited_date=).with("21/07/2010")
-        @paper.should_receive(:department=).with("Department for Transport")
+        @paper.should_receive(:department=).with(["Department for Transport"])
         @paper.should_receive(:link_to_paper=).with("http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip")
         @paper.should_receive(:description=).with("Description goes here")
         @paper.should_receive(:legislation=).with("")
@@ -131,7 +131,7 @@ describe "Parser" do
         DepositedPaper.should_receive(:new).with("DEP07-2064").and_return(@paper)
         
         @paper.should_receive(:deposited_date=).with("08/10/2007")
-        @paper.should_receive(:department=).with("Treasury")
+        @paper.should_receive(:department=).with(["Treasury"])
         @paper.should_receive(:description=).with("Tables showing lone parents of working age with dependent children as a proportion of the working population, taken from the 1981, 1991 and 2001 censuses.")
         @paper.should_receive(:legislature=).with("")
         @paper.should_receive(:legislation=).with("")
@@ -142,5 +142,319 @@ describe "Parser" do
       end
     end
     
+    describe "when parsing multiple department names" do
+      before do
+        @parser = Parser.new("2009")
+        
+        @paper.stub!(:legislature=)
+        @paper.stub!(:deposited_date=)
+        @paper.stub!(:link_to_paper=)
+        @paper.stub!(:description=)
+        @paper.stub!(:legislation=)
+        @paper.stub!(:notes=)
+        
+        DepositedPaper.should_receive(:new).with("DEP2010-1518").and_return(@paper)
+      end
+      
+      it 'should split the string on "Office for"' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Education Board Office for National Statistics</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+        
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Education Board", "Office for National Statistics"])
+        
+        @parser.parse()
+      end
+      
+      it 'should split the string on "Office of"' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Education Board Office of Fair Trading</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+        
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        
+        @paper.should_receive(:department=).with(["Education Board", "Office of Fair Trading"])
+        
+        @parser.parse()
+      end
+      
+      it 'should split the string on "Dept of"' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Education Board Dept of Health</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+        
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Education Board", "Department of Health"])
+        
+        @parser.parse()
+      end
+      
+      it 'should split the string on "Ministry of"' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Youth Justice Board Ministry of Justice</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+        
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Youth Justice Board", "Ministry of Justice"])
+        
+        @parser.parse()
+      end
+
+      it 'should split the string after "Cabinet Office"' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Cabinet Office Youth Justice Board</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Cabinet Office", "Youth Justice Board"])
+
+        @parser.parse()
+      end
+      
+      it 'should split the string after "Speaker"' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">speaker Cabinet Office Youth Justice Board</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Office of the Speaker", "Cabinet Office", "Youth Justice Board"])
+
+        @parser.parse()
+      end
+      
+      it 'should split the string after "Prime Minister"' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Prime Minister Youth Justice Board</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Prime Minister", "Youth Justice Board"])
+
+        @parser.parse()
+      end
+      
+      it 'should split the string "Prime Minister Ministry of Justice" correctly' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Prime Minister Ministry of Justice</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Prime Minister", "Ministry of Justice"])
+
+        @parser.parse()
+      end
+      
+      it 'should split the string "Cabinet Office Central Office of Information" correctly' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Cabinet Office Central Office of Information</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Cabinet Office", "Central Office of Information"])
+
+        @parser.parse()
+      end
+      
+      it 'should split the string "Home Office Government Equalities Office" correctly' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Home Office Government Equalities Office</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Home Office", "Government Equalities Office"])
+
+        @parser.parse()
+      end
+      
+      it 'should split the string "Dept for Environment, Food and Rural Affairs Environment Agency Wales" correctly' do
+        html = '<html><table class="DP"><tr valign="top">
+          <td class="ref">DEP2010-1518</td>
+          <td class="leg">Commons, Lords</td>
+          <td class="date">21/07/2010</td>
+          <td colspan="1" class="dept">Dept for Environment, Food and Rural Affairs Environment Agency Wales</td>
+          <td class="link"><A HREF="http://www.parliament.uk/deposits/depositedpapers/2010/DEP2010-1518.zip">DEP2010-1518.ZIP</A></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="des" colspan="2">Description goes here</td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="legislation" colspan="2"><i></i></td>
+          </tr>
+          <tr>
+          <td colspan="3"></td>
+          <td class="notes" colspan="2"></td>
+          </tr></table></html>'
+
+        @parser.should_receive(:open).with('http://deposits.parliament.uk/deposited_papers.asp?year=2009').and_return(html)
+        @paper.should_receive(:department=).with(["Department for Environment, Food and Rural Affairs", "Environment Agency Wales"])
+
+        @parser.parse()
+      end  
+      
+    end
   end
+    
 end
